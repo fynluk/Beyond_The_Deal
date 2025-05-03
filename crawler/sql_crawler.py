4dcb29b623022b463ac8c2e388ded85433d42c16
fynluk <spaeker@23technologiescloud.cloud>
2025-05-03 21:04:47 +0200

    * Drafted getTickers * Build Upload Function for historical Data

--- crawler/sql_crawler.py
+++ crawler/sql_crawler.py
@@ -1,5 +1,7 @@
 import mysql.connector
 import logging
+
+import pandas._libs.missing
 from IPython.core.display_functions import display
 
 def initDatabase(db, stock):
@@ -28,7 +30,47 @@ class SqlHandler:
         except:
             logging.error("SQL Bridge connection failed")
 
-    def init(self, deals):
+    def init(self, ref,  deals):
         for deal in deals:
             initDatabase(self, deal.buyer)
             initDatabase(self, deal.target)
+
+    def getTickers(self):
+        cursor = self.db.cursor(buffered=True)
+        cursor.execute("SHOW TABLES")
+        tickers = [t[0] for t in cursor.fetchall()]
+        tickersToFetch = []
+        for ticker in tickers:
+            #cursor.execute("SELECT * FROM " + ticker)
+            if True:
+                # TODO Only return tickers where data is missing
+                tickersToFetch.append(ticker)
+        return tickersToFetch
+
+    def uploadData(self, ticker, date, open, high, low, close, vol):
+        # Check whether Data exists for the date
+        cursor = self.db.cursor(buffered=True)
+        check_query = f"SELECT 1 FROM `{ticker}` WHERE `Date` = %s LIMIT 1"
+        cursor.execute(check_query, (date,))
+        exists = cursor.fetchone()
+        if exists:
+            # Datapoint exists
+            pass
+        else:
+            # If value has type pd.NA use Value 0.0
+            def sanitize(value):
+                return 0.0 if type(value) == pandas._libs.missing.NAType else value
+
+            # Werte bereinigen
+            open = sanitize(open)
+            high = sanitize(high)
+            low = sanitize(low)
+            close = sanitize(close)
+            vol = sanitize(vol)
+
+            insert_query = f"""
+                        INSERT INTO `{ticker}` (Date, Open, High, Low, Close, Volume)
+                        VALUES (%s, %s, %s, %s, %s, %s)
+                    """
+            cursor.execute(insert_query, (date, open, high, low, close, vol))
+            self.db.commit()
